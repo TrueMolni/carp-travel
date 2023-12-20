e95eebcbd8d04b1b290037a53997d2644b4a0410
TrueMolni <truemolni@gmail.com>
2023-12-20 21:19:51 +0200

    feature/markup-02
    add Logo markup

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 // import logo from "../images/CarpTravelLogo.svg";
 // import logo from "../images/CarpTravelLogoPng.png";
 
 const Logo = () => {
   //   console.log(logo);
-  return <Link href={"/"}>Logo</Link>;
+  return (
+    <Link className="flex" href={"/"}>
+      <Image src={"/Frame.svg"} alt="Logo" width={61} height={33} />
+      <span className="text-[#fff]">CarpTravel</span>
+    </Link>
+  );
 };
 
 export default Logo;
+/* 
+color: #FFF;
+
+leading-trim: both;
+
+text-edge: cap;
+font-family: Karantina;
+font-size: 14px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+letter-spacing: 2.59px;
+*/
